15aeb0db30dcab76aa99e8e5b9db5c6050a238cc
Ivan <raggamix@mail.ru>
2021-02-24 15:11:58 +0300

    feat: add style components

--- tetris/src/components/Tetris.js
+++ tetris/src/components/Tetris.js
@@ -1,6 +1,7 @@
 import React from 'react';
 
 import { createStage } from '../gameHelpers';
+import { StyledTetrisWrapper, StyledTetris } from './styles/StyledTetris';
 
 import Stage from './Stage';
 import Display from './Display';
@@ -9,7 +10,8 @@ import StartButton from './StartButton';
 const Tetris = () => {
 
     return (
-        <div>
+        <StyledTetrisWrapper>
+            <StyledTetris>
             <Stage stage={createStage()} />
             <aside>
                 <div>
@@ -19,7 +21,8 @@ const Tetris = () => {
                 </div>
                 <StartButton />
             </aside>
-        </div>
+            </StyledTetris>
+        </StyledTetrisWrapper>
     );
 };
 
